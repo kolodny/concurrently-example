56489f31bd84ee9df01a9a320ed30e3b98e1f1de
Moshe Kolodny <kolodny@github.com>
2017-04-30 00:46:16 -0400

    commit

---
+++ foo.js
@@ -0,0 +1,17 @@
+process.stdin.resume();//so the program will not close instantly
+
+function exitHandler(options, err) {
+    console.log('exitHandler');
+    if (options.cleanup) console.log('clean');
+    if (err) console.log(err.stack);
+    if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
